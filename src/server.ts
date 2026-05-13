6b6cf45974ebbd12a8fc66c91d2db806c7e948cb
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2026-05-13 19:29:09 +0600

    Create server with express & Typescript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-const some =1
+import express from "express"
+const app = express()
+const port = 5000
+
+app.get('/user', (req, res) => {
+  res.send('Hello World!!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
