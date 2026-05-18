ecb9665aa6638272697cf5291792e9939903e630
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2026-05-18 19:24:47 +0600

    setting the namespace

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,52 +6,65 @@ import { pool } from "../db";
 
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    // console.log("This is protected route");
-    // console.log(req.headers.authorization)
-    const token = req.headers.authorization;
+    try {
+      // console.log("This is protected route");
+      // console.log(req.headers.authorization)
 
-    console.log(token);
+      //* 1. Check if the token exists
 
-    if (!token) {
-      res.status(401).json({
-        success: false,
-        message: "Unauthorized access!!",
-      });
-    }
-    const decoded = jwt.verify(
-      token as string,
-      config.secret as string,
-    ) as JwtPayload;
+      const token = req.headers.authorization;
 
-    const userData = await pool.query(
-      `
+      console.log(token);
+
+      if (!token) {
+        res.status(401).json({
+          success: false,
+          message: "Unauthorized access!!",
+        });
+      }
+
+      //* 2. Verify the token
+
+      const decoded = jwt.verify(
+        token as string,
+        config.secret as string,
+      ) as JwtPayload;
+
+      //* 3. Find the user into database
+
+      const userData = await pool.query(
+        `
      SELECT * FROM users WHERE email=$1   
         `,
-      [decoded.email],
-    );
+        [decoded.email],
+      );
 
-    // console.log(userData);
+      // console.log(userData);
 
-    const user = userData.rows[0];
+      const user = userData.rows[0];
 
-    // console.log(user)
-    if (userData.rows.length === 0) {
-      res.status(404).json({
-        success: false,
-        message: "User not found!",
-      });
-    }
+      // console.log(user)
+      if (userData.rows.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: "User not found!",
+        });
+      }
 
-    if(!user.is_active){
+      //* 4. If the user active or not?
+      if (!user.is_active) {
         res.status(403).json({
           success: false,
           message: "Forbidden!!",
         });
+      }
+
+      req.user = decoded; // req : { user : {} }
+
+      next();
+    } catch (error) {
+      next(error);
     }
-
-    
-
-    next();
   };
 };
 
